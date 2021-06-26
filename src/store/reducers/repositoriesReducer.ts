51c882bb0537e7663818f6226fab9d3a0fb11dd9
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2021-06-26 18:56:02 +0200

    create repoReducer

---
+++ src/store/reducers/repositoriesReducer.ts
@@ -0,0 +1,33 @@
+import { ActionType } from "../action-types/";
+import { Action } from "../actions";
+
+interface RepoState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const initialState = {
+  loading: false,
+  error: null,
+  data: [],
+};
+
+export const repoReducer = (
+  state: RepoState = initialState,
+  action: Action
+): RepoState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+
+    default:
+      return state;
+  }
+};
